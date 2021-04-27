9c91a7af5fec021b5f3d41158bf52142a02729be
ayrton-vitor <ayrtonvitor@usp.br>
2021-04-27 19:31:40 -0300

    debug k_means

--- Representative_based_Clustering/k_means.py
+++ Representative_based_Clustering/k_means.py
@@ -1,34 +1,47 @@
 import numpy as np
 import pandas as pd
 
-def k_means(data, k, threshold=0):
+def k_means(data, k):
     #initialize centroids
     centroids = np.empty((k,data.shape[1]))
     centroids = pd.DataFrame(centroids, columns=data.columns)
-    for col in data.columns:
-        centroid = np.random.uniform(data[col].min(), data[col].max, k)
-        centroids[col] = centroid
+    centroids = pd.DataFrame(generate_random_centroids(data, k))
     
     #for each point, indicates which centroid it is associated with and their distance
     group = pd.DataFrame({'distance': np.full(data.shape[0], float('inf')), 'centroid': np.arange(data.shape[0])})
-    error = prev_error = group['distance'].sum()
-
+    error = group['distance'].sum()
+    prev_error = 0
+    
+    too_big = True
     #minimizes SSE
-    while error > threshold or error > prev_error:
+    while too_big:
         #asign points to closest centroid
         for i in range(centroids.shape[0]):
             centroid = centroids.loc[i]
             distances = ((data - centroid) ** 2).sum(axis=1) #finds distances from points to centroid
             closer_points = group['distance'] > distances
-            group[closer_points]['centroid'] = i
-            group[closer_points['distance']] = distances[closer_points]
-
+            group.loc[closer_points, 'centroid'] = i
+            group.loc[closer_points, 'distance'] = distances[closer_points]
+                
         #computes new centroids
         for i in range(k):
             cluster = data[group['centroid'] == i]
-            centroids.loc[i] = cluster.sum() / cluster.shape[0]
+            if cluster.empty:
+                centroids.loc[i] = next(generate_random_centroids(data, 1))
+                #this does not guarantee that the new centroid will be
+                #different from all other vectors, but the probability
+                #of such occurence is slim
+            else:
+                centroids.loc[i] = cluster.sum() / cluster.shape[0]
 
         prev_error = error
         error = group['distance'].sum()
+        too_big = error < prev_error
     
-    return group['centroid'], error
+    return group['centroid'], error, centroids
+
+
+def generate_random_centroids(df, k):
+    for i in range(k):
+        num = np.random.rand(df.shape[0], 1)
+        yield (df * num).sum() / num.sum()
