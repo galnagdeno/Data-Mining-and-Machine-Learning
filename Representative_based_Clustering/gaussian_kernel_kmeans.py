1bbab87fb0b44979d3d1c311048d3cfd2367f32e
ayrton-vitor <ayrtonvitor@usp.br>
2021-05-06 23:55:44 -0300

    implement gaussian kernel clustering algorithm

---
+++ Representative_based_Clustering/gaussian_kernel_kmeans.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import scipy
+from kernel_kmeans import kernel_kmeans
+from scipy.spatial.distance import pdist, squareform
+
+def gaussian_kmeans(df, var, num_clusters):
+    kernel_matrix = scipy.exp(- squareform(pdist(df, 'euclidian')) / var ** 2)
+    kernel_matrix = pd.DataFrame(kernel_matrix, columns = range(df.shape[0]))
+    return kernel_kmeans(kernel_matrix, num_clusters)
+    
