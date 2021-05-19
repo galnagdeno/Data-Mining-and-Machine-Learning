4f0cfe16b07c9d45657ab532cbb3ae2501fd4aaf
ayrton-vitor <ayrtonvitor@usp.br>
2021-05-18 21:42:10 -0300

    Fix error in pdist calculation call

--- Representative_based_Clustering/gaussian_kernel_kmeans.py
+++ Representative_based_Clustering/gaussian_kernel_kmeans.py
@@ -4,7 +4,7 @@ from kernel_kmeans import kernel_kmeans
 from scipy.spatial.distance import pdist, squareform
 
 def gaussian_kmeans(df, var, num_clusters):
-    kernel_matrix = scipy.exp(- squareform(pdist(df, 'euclidian')) / var ** 2)
+    kernel_matrix = scipy.exp(- squareform(pdist(df, 'euclidean')) / var ** 2)
     kernel_matrix = pd.DataFrame(kernel_matrix, columns = range(df.shape[0]))
     return kernel_kmeans(kernel_matrix, num_clusters)
     
